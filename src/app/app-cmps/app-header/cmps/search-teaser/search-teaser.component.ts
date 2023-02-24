4aad87419330b7953431b40d12a43e9c40ef31c7
Boydem <noyemdahan@gmail.com>
2023-02-24 18:55:54 +0200

    added image carousel states and markup

--- src/app/app-cmps/app-header/cmps/search-teaser/search-teaser.component.ts
+++ src/app/app-cmps/app-header/cmps/search-teaser/search-teaser.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'search-teaser',
   templateUrl: './search-teaser.component.html',
-  styleUrls: ['./search-teaser.component.scss']
+  styleUrls: ['./search-teaser.component.scss'],
 })
 export class SearchTeaserComponent {
-
+  faSearch = faSearch;
 }
--- src/app/pages/home/cmps/img-carousel/img-carousel.component.ts
+++ src/app/pages/home/cmps/img-carousel/img-carousel.component.ts
@@ -1,10 +1,18 @@
 import { Component, Input } from '@angular/core';
-
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'img-carousel',
   templateUrl: './img-carousel.component.html',
   styleUrls: ['./img-carousel.component.scss'],
 })
 export class ImgCarouselComponent {
+  faChevronRight = faChevronRight;
+  faChevronLeft = faChevronLeft;
   @Input() imgUrls!: string[];
+  currentIndex: number = 0;
+
+  goToPrev() {}
+  goToNext() {}
+  goToIndex(index: number) {}
 }
