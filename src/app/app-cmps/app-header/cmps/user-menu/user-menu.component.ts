b9b3312c9eb053d2e3fc0d1dffe03d0985562689
Boydem <noyemdahan@gmail.com>
2023-02-27 23:56:21 +0200

    added user icon

--- src/app/app-cmps/app-header/cmps/user-menu/user-menu.component.ts
+++ src/app/app-cmps/app-header/cmps/user-menu/user-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
+  styleUrls: ['./user-menu.component.scss'],
 })
 export class UserMenuComponent {
-
+  faUser = faUser;
 }
